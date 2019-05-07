643af0731c0043567b9266d10bf3c2272a1d125e
Hebert Santos <hb-santos@hotmail.com>
2019-05-07 18:44:44 -0300

    MigrationTwo

--- SafePets/Data/ApplicationDbContext.cs
+++ SafePets/Data/ApplicationDbContext.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SafePets.Models;
 
 namespace SafePets.Data
 {
@@ -12,5 +13,6 @@ namespace SafePets.Data
             : base(options)
         {
         }
+        public DbSet<SafePets.Models.Pet> Pet { get; set; }
     }
 }
---
+++ SafePets/Models/Pet.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SafePets.Models
+{
+    public class Pet
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Raca { get; set; }
+        public char Sexo { get; set; }
+        public string Idade { get; set; }
+        public byte[] Imagem { get; set; }
+    }
+}
