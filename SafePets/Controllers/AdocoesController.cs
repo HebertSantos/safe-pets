7e1a0d6bb05fec98e6a3a53da60fe0c23958bae0
Hebert Santos <hb-santos@hotmail.com>
2019-06-12 01:20:16 -0300

    relatoriobugado

--- SafePets/Controllers/AdocoesController.cs
+++ SafePets/Controllers/AdocoesController.cs
@@ -3,18 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
+using SafePets.Services;
 namespace SafePets.Controllers
 {
     public class AdocoesController : Controller
     {
+        private readonly AdocoesService _adocoesService;
+
+        public AdocoesController(AdocoesService adocoesService)
+        {
+            _adocoesService = adocoesService;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult ConsultaSimples()
+        public async Task<IActionResult> ConsultaSimples(DateTime? minDate, DateTime maxDate)
         {
+            var result = await _adocoesService.FindByDateAsync(minDate,maxDate);
             return View();
         }
 
