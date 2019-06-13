aa5afd720321872415f6b5d73a2883172a25e4a5
Hebert Santos <hb-santos@hotmail.com>
2019-06-13 00:17:29 -0300

    Finalagora

---
+++ SafePets/Data/Migrations/20190613025634_final.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SafePets.Data.Migrations
+{
+    public partial class final : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sexo",
+                table: "Pet",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Raca",
+                table: "Pet",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Pet",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Idade",
+                table: "Pet",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Sexo",
+                table: "Pet",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Raca",
+                table: "Pet",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Pet",
+                nullable: true,
+                oldClrType: typeof(string));
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Idade",
+                table: "Pet",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
--- SafePets/Models/Pet.cs
+++ SafePets/Models/Pet.cs
@@ -2,15 +2,23 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using System.ComponentModel.DataAnnotations;
 namespace SafePets.Models
 {
     public class Pet
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "Preencha o campo Nome")]
+        [Display(Name = "Nome")]
         public string Nome { get; set; }
+        [Required(ErrorMessage = "Preencha o campo Raça")]
+        [Display(Name = "Raça")]
         public string Raca { get; set; }
+        [Required(ErrorMessage = "Selecione o Sexo do Animal")]
+        [Display(Name = "Sexo")]
         public string Sexo { get; set; }
+        [Required(ErrorMessage = "Preencha o campo Idade")]
+        [Display(Name = "Idade")]
         public string Idade { get; set; }
         public ICollection<Adocao> Adocoes { get; set; } = new List<Adocao>();
 
